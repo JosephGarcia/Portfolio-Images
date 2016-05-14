4e6ec2f643b51856e71ed47461d31b174ecaebbb
Joseph Garcia <j.garc419@gmail.com>
2016-05-13 21:36:52 -0400

    learning how to show directions on a MapView

---
+++ MKDirection/MapKitDirectionDemo/MapViewController.swift
@@ -0,0 +1,145 @@
+//
+//  MapViewController.swift
+//  MapKitDirectionDemo
+//
+//  Created by Simon Ng on 18/11/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView:MKMapView!
+    
+    @IBAction func getDirections(sender: AnyObject) {
+        
+        guard let currentPlacemark = currentPlacemark else {
+            return
+        }
+        
+        let directionRequest = MKDirectionsRequest()
+        
+        directionRequest.source = MKMapItem.mapItemForCurrentLocation()
+        let destinationPlacemark = MKPlacemark(placemark: currentPlacemark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlacemark)
+        directionRequest.transportType = MKDirectionsTransportType.Automobile
+        
+        //CALCULATE DIRECTIONS
+        let directions = MKDirections(request: directionRequest)
+        
+        directions.calculateDirectionsWithCompletionHandler { (routeResponse, routeError) -> Void in
+            
+            guard let routeResponse = routeResponse else {
+                if let routeError = routeError {
+                    print("ERROR \(routeError)")
+                }
+                return
+            }
+            let route = routeResponse.routes[0]
+            self.mapView.addOverlay(route.polyline, level:  MKOverlayLevel.AboveRoads)
+        }
+    }
+    
+    
+    var restaurant:Restaurant!
+    let locationManager = CLLocationManager()
+    var currentPlacemark: CLPlacemark?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
+
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
+        // Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.address, completionHandler: { placemarks, error in
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+                self.currentPlacemark = placemark
+                
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.category
+                
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    // Display the annotation
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
+
+    }
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blueColor()
+        renderer.lineWidth = 3.0
+        
+        return renderer
+    }
+
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyPin"
+        
+        if annotation.isKindOfClass(MKUserLocation) {
+            return nil
+        }
+        
+        // Reuse the annotation if possible
+        var annotationView:MKPinAnnotationView? = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+        }
+        
+        let leftIconView = UIImageView(frame: CGRectMake(0, 0, 53, 53))
+        leftIconView.image = UIImage(named: restaurant.image)!
+        annotationView?.leftCalloutAccessoryView = leftIconView
+        
+        // Pin color customization
+        if #available(iOS 9.0, *) {
+            annotationView?.pinTintColor = UIColor.orangeColor()
+        }
+        
+        return annotationView
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
